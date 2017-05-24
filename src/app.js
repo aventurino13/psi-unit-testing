f01e95d5e1c4fc8ddc1df44c038747e44f30016e
Amy Venturino <aventuri@umich.edu>
2017-05-24 14:36:14 -0500

    removed body-parser

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var app = express();
 var addOne = require('./modules/addOne');
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
 
 
 // GET
